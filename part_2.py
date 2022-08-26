2ddefef768c967b21cacb2cfb71015ecf62c7b4b
Lekha Sathvik Devabathini <dlsathvik04@gmail.com>
2022-08-26 15:47:56 +0530

    last commit

--- part_2.py
+++ part_2.py
@@ -40,7 +40,6 @@ def talk(Word : str):
     engine = pyttsx3.init()
     engine.say(Word)
     engine.runAndWait()
-    return Word
     
 #'''-------------------------------------------------------------------------------------------------------------------------------------------------------------------------'''
     
@@ -79,11 +78,24 @@ def dictate_10_words(level : int):
     return words
 
 #'''-------------------------------------------------------------------------------------------------------------------------------------------------------------------------'''
-tab1, tab2, tab3 = st.tabs(["Home", "pronounciation test", "listening test"])
+
+def random_seq():
+    list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
+    return " ".join(random.sample(list, 5))
+
+#'''-------------------------------------------------------------------------------------------------------------------------------------------------------------------------'''
+
+
+
+#'''-------------------------------------------------------------------------------------------------------------------------------------------------------------------------'''
+
+tab1, tab2, tab3 = st.tabs(["Home", "pronounciation test", "memory test"])
+
 level = 1
 
+
 with tab1:
-    
+    st.title("A Test for Dyslexia")
     option = st.selectbox(
             "select your standard", ('2nd-4th', '5th-7th'), key= "pro")
     if option=='2nd-4th':
@@ -92,13 +104,14 @@ with tab1:
         level = 1
 
 with tab2:
-    st.title("A Test for Dyslexia")
     st.header("The pronounciation and reading ability of the user will be measured here")
     pronounciation_test = st.button("Start a pronouncation test")
 
-    pronounciation_inaccuracy = 0 
+    pronounciation_inaccuracy = 0
+
+           
     if pronounciation_test:
-        st.subheader("Please repeate the following words you only has 20 seconds to do that.")
+        st.subheader("Please repeate the following words you only has 10 seconds to do that.")
          
         arr = get_10_word_array(level)
         for i in range(len(arr)):
@@ -108,20 +121,50 @@ with tab2:
         str_displayed = str(" ".join(arr))
         words = st.text(">> " + "\n>>".join(arr) )
         status = st.text("listenning........")
-        try:
-            str_pronounced = listen_for(30)
-            status.write("Time up! calculating inacuracy......")
-            # str_displayed = " ".join(arr)
-            pronounciation_inaccuracy = check_pronounciation(str_displayed, str_pronounced)/len(str_displayed)
-            words.write("the pronounciation inacuuracy is: " + str(pronounciation_inaccuracy))
-        except:
-            status.write("apparantly you spoke nothing...") 
+        str_pronounced = listen_for(10)
+        status.write("Time up! calculating inacuracy......")
         
-    
-      
-word_array = get_10_word_array(level)
- 
-# with tab3: 
-      
+        
+        pronounciation_inaccuracy = check_pronounciation(str_displayed, str_pronounced)/len(str_displayed)
+        
+        words.write("the pronounciation inacuuracy is: " + str(pronounciation_inaccuracy))
+        status.write("original : " + ipa.convert(str_displayed) )
+        st.write("\npronounced: " + ipa.convert(str_pronounced))
+        
 
+
+    
+# with tab3:
+#     st.write("Now when you click this button you will start listening 10 words one by one please pay attention and type all those words in the field below with spaces in between. System wont repeat words.")
+#     start_listening = st.button("Start My test")
+#     str = st.text_input("enter the words you are listening")
+#     dictate_10_words(level)
+#     print(str)
+#     st.write("the words are completed please click enter")
+#     time.sleep(5)
+#     st.write(str)
+#     st.write(dictated_words)
+  
+# @st.cache(suppress_st_warning=True)
+# def bind_socket():
+#     string =  random_seq()
+#     random_str = st.subheader(string)
+#     time.sleep(5)
+#     random_str.write("")
+    
+    
+              
+# with tab3:
+#     st.header("Memory Test")
+#     st.write("a sequence of 5 characters will be displayed for 5 seconds please try to remember and reproduce it later.")
+#     start_memory_t = st.button("Start memory Test")
+#     if start_memory_t:
+#         bind_socket()
+        
+        
+            
+    
+    
+    
+    
     
